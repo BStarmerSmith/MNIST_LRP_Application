4993040fda1892a71674302245a22ce1666f4d12
Ben Starmer-Smith <benlfstarmersmith@gmail.com>
2021-02-03 16:35:29 +0000

    Fixed LRP, works smoothly
    Signed-off-by: Ben Starmer-Smith <benlfstarmersmith@gmail.com>

--- src/data_loader.py
+++ src/data_loader.py
@@ -7,7 +7,6 @@ from PIL import ImageEnhance, ImageOps, Image
 import copy
 from torch.autograd import Variable
 from torchvision import transforms
-import torchvision
 
 MINST_MEAN, MINST_STANDARD_DIV = 0.1307, 0.3081
 transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((MINST_MEAN,), (MINST_STANDARD_DIV,))])
@@ -70,53 +69,73 @@ def preform_lrp(image):
     show_image_tensor(image_tensor)
     print(e_lrp(model, image_tensor))
 
+
 def e_lrp(model, img_tensor):
-    layers = list(model._modules['layer1']) + list(model._modules['layer2'])
-    layers.append(toconv(model._modules['fc1']))
-    layers.append(toconv(model._modules['fc2']))
+    layers = list(model._modules['layer1']) + list(model._modules['layer2']) \
+             + toconv([model._modules['fc1'], model._modules['fc2']])
 
     L = len(layers)
     A = [img_tensor]+[None]*L
-
     for l in range(L):
         A[l+1] = layers[l].forward(A[l].to(device))
 
-    T = A[-1].detach().numpy().tolist()[0]
+    T = A[-1].cpu().detach().numpy().tolist()[0]
     index = T.index(max(T))
     T = np.abs(np.array(T)) * 0
     T[index] = 1
     T = torch.FloatTensor(T)
 
-    R = [None]*L + [(A[-1]*T).data]
+    R = [None] * L + [(A[-1].cpu() * T).data + 1e-6]
 
     for l in range(1,L)[::-1]:
         A[l] = (A[l].data).requires_grad_(True)
-
         if isinstance(layers[l],torch.nn.MaxPool2d): layers[l] = torch.nn.AvgPool2d(2)
-
         if isinstance(layers[l],torch.nn.Conv2d) or isinstance(layers[l],torch.nn.AvgPool2d):
-            if l <= 5: rho = lambda p: p + 0.25*p.clamp(min=0); incr = lambda z: z+1e-9
-            else: rho = lambda p: p; incr = lambda z: z+1e-9
+
+            if l <= 2:       rho = lambda p: p + 0.25*p.clamp(min=0); incr = lambda z: z+1e-9
+            if 3 <= l <= 5: rho = lambda p: p;                       incr = lambda z: z+1e-9+0.25*((z**2).mean()**.5).data
+            if l >= 6:       rho = lambda p: p;                       incr = lambda z: z+1e-9
 
             z = incr(newlayer(layers[l],rho).forward(A[l]))  # step 1
-            s = (R[l + 1]/z).data                            # step 2
-            (z*s).sum().backward(); c = A[l].grad            # step 3
-            R[l] = (A[l]*c).data                             # step 4
-            print(R[l], "ahh")
+            s = (R[l+1].to(device)/z).data                                    # step 2
+            (z*s).sum().backward(); c = A[l].grad                  # step 3
+            R[l] = (A[l]*c).data                                   # step 4
         else:
             R[l] = R[l+1]
-        return R[0]
 
-def toconv(layer):
-    newlayer = None
-    layer.
-    if isinstance(layer, torch.nn.Linear):
-        m,n = 64, layer.weight.shape[0]
-        newlayer = torch.nn.Conv2d(m,n,14)
-        newlayer.weight = torch.nn.Parameter(layer.weight.reshape(1,m,14,14))
-        newlayer.bias = torch.nn.Parameter(layer.bias)
-    return newlayer
+    mean, std = 0.1307, 0.3081
 
+    A[0] = (A[0].data).requires_grad_(True)
+    lb = (A[0].data*0+(0-mean)/std).requires_grad_(True)
+    hb = (A[0].data*0+(1-mean)/std).requires_grad_(True)
+
+    z = layers[0].forward(A[0].to(device)) + 1e-9                                     # step 1 (a)
+    z -= newlayer(layers[0],lambda p: p.clamp(min=0)).forward(lb.to(device))    # step 1 (b)
+    z -= newlayer(layers[0],lambda p: p.clamp(max=0)).forward(hb.to(device))    # step 1 (c)
+    s = (R[1]/z).data                                                      # step 2
+    (z*s).sum().backward(); c,cp,cm = A[0].grad,lb.grad,hb.grad            # step 3
+    R[0] = (A[0]*c+lb*cp+hb*cm).data
+    heatmap(np.array(R[0][0]).sum(axis=0),3.5,3.5)
+
+
+def toconv(layers):
+    newlayers = []
+    for i,layer in enumerate(layers):
+        if isinstance(layer, torch.nn.Linear):
+            newlayer = None
+            if i == 0:
+                m,n = 64,layer.weight.shape[0]
+                newlayer = torch.nn.Conv2d(m,n,7)
+                newlayer.weight = torch.nn.Parameter(layer.weight.reshape(n,m,7,7))
+            else:
+                m,n = layer.weight.shape[1],layer.weight.shape[0]
+                newlayer = torch.nn.Conv2d(m,n,1)
+                newlayer.weight = torch.nn.Parameter(layer.weight.reshape(n,m,1,1))
+            newlayer.bias = torch.nn.Parameter(layer.bias)
+            newlayers += [newlayer]
+        else:
+            newlayers += [layer]
+    return newlayers
 
 def newlayer(layer, g):
     layer = copy.deepcopy(layer)
@@ -163,3 +182,14 @@ def show_image(image, figsize=(8, 4), title=None):
     if title: plt.title(title)
     plt.show()
 
+def heatmap(R,sx,sy):
+    b = 10*((np.abs(R)**3.0).mean()**(1.0/3))
+    from matplotlib.colors import ListedColormap
+    my_cmap = plt.cm.seismic(np.arange(plt.cm.seismic.N))
+    my_cmap[:,0:3] *= 0.85
+    my_cmap = ListedColormap(my_cmap)
+    plt.figure(figsize=(sx,sy))
+    plt.subplots_adjust(left=0,right=1,bottom=0,top=1)
+    plt.axis('off')
+    plt.imshow(R,cmap=my_cmap,vmin=-b,vmax=b,interpolation='nearest')
+    plt.show()
